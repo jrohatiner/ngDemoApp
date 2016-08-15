a14a55deea28a4b79bb5ba5a06768e1d78cb5843
Rey Ramos <reymundo.ramos@gmail.com>
2016-08-15 19:29:35 -0400

    updates

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component}          from '@angular/core';
+
+import './rxjs-extensions.ts';
+
+
+@Component({
+    selector: 'my-app',
+    template: require('./app.component.html')
+})
+
+
+export class AppComponent {
+    title = 'LabCorp Title';
+}
