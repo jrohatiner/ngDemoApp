716ed913a2a9c92415d94c4d180d00f009ae03eb
Rey Ramos <reymundo.ramos@gmail.com>
2016-08-17 19:14:09 -0400

    update to make smaller

--- src/app/patients/patients.service.ts
+++ src/app/patients/patients.service.ts
@@ -3,99 +3,103 @@
  */
 import {Injectable}             from '@angular/core';
 import {Http, Response}         from '@angular/http'
-
+import {Observable}             from 'rxjs/Rx';
 
 
 export class Patients {
-	constructor(public id: number,
-	            public firstName: string,
-	            public lastName: string,
-	            public imageUrl: string,
-	            public city?: string,
-	            public email?: string,
-	            public addressLine1?: string,
-	            public phone?: string,
-	            public state?: string,
-	            public zip?: number) {
-	}
+    constructor(public id: number,
+                public firstName: string,
+                public lastName: string,
+                public imageUrl: string,
+                public city?: string,
+                public email?: string,
+                public addressLine1?: string,
+                public phone?: string,
+                public state?: string,
+                public zip?: number) {
+    }
 }
 
 
 @Injectable()
 export class PatientService {
-	static instance: PatientService;
+    static instance: PatientService;
 
-	patients: Array<any> = [];
+    patients: Array<any> = [];
 
-	constructor(private http: Http) {
+    constructor(private http: Http) {
 
-		return PatientService.instance = PatientService.instance || this;
+        return PatientService.instance = PatientService.instance || this;
 
-	}
-
-
-	// Uses http.get() to load a single JSON file
-	getPatients() {
-		let self = this;
+    }
 
 
-		return this.http.get('api/patients.json').map((res: Response) => {
-			//reset
-			self.patients = [];
-			res.json().patientList.forEach((obj: any, idx: number) => {
-				let p = obj.patient;
-
-				let min: number = Math.ceil(1);
-				let max: number = Math.floor(4);
+    // Uses http.get() to load a single JSON file
+    getPatients() {
+        let self = this;
 
 
-				self.patients.push(new Patients(
-					idx,
-					p.firstName,
-					p.lastName,
-					['images/avatars/' + (idx % 2 ? 'male' : 'female'),
-						'avatar',
-						//this is to force a new http for every image being loaded, simulate proper images, since
-						//everyone don't look the same.
-						(Math.floor(Math.random() * (max - min)) + min)].join('-') + '.png?' + (Date.now() + idx)
-				))
-			});
+        return this.http.get('api/patients.json').map((res: Response) => {
+            //reset
+            self.patients = [];
+            res.json().patientList.forEach((obj: any, idx: number) => {
+                let p = obj.patient;
 
-			return self.patients;
-		});
-	}
+                let min: number = Math.ceil(1);
+                let max: number = Math.floor(4);
 
 
-	//returning a promise
-	getDetails(id: any): Promise<any> | any {
+                self.patients.push(new Patients(
+                    idx,
+                    p.firstName,
+                    p.lastName,
+                    ['images/avatars/' + (idx % 2 ? 'male' : 'female'),
+                        'avatar',
+                        //this is to force a new http for every image being loaded, simulate proper images, since
+                        //everyone don't look the same.
+                        (Math.floor(Math.random() * (max - min)) + min)].join('-') + '.png?' + (Date.now() + idx)
+                ))
+            });
 
-		let self = this;
+            return self.patients;
+        });
+    }
 
-		return Promise.all([
-			this.http.get('api/invoices.json').toPromise().then(response => response.json()),
-			this.http.get('api/labResults.json').toPromise().then(res => res.json()),
-			this.http.get('api/patient.json').toPromise().then(res => res.json()),
-			this.http.get('api/physician.json').toPromise().then(res => res.json()),
-			this.http.get('api/prescriptions.json').toPromise().then(res => res.json()),
-			this.http.get('api/visitations.json').toPromise().then(res => res.json())
-		]).then(data => {
-			        let api = {
-			            'invoices': data[0]['invoices'],
-			            'labResults': data[1]['tests'],
-			            'patient': function () {
-			                return Object.assign({}, data[2], {imageUrl: self.patients.length ? self.patients[id].imageUrl : 'images/placeholder.svg'});
-			            }(),
-			            'physician': function () {
-			                return Object.assign({}, data[3], {imageUrl: 'images/doctor-01.svg'});
-			            }(),
-			            'prescriptions': data[4]['prescriptions'],
-			            'visitations': data[5]['visitations']
-			        };
 
-			        return api;
-		});
+    //returning a promise
+    getDetails(id: any): Promise<any> | any {
 
-	}
+        let self = this;
+
+        return new Promise((resolve) => {
+            Observable.forkJoin(
+                this.http.get('api/invoices.json').toPromise().then(response => response.json()),
+                this.http.get('api/labResults.json').toPromise().then(res => res.json()),
+                this.http.get('api/patient.json').toPromise().then(res => res.json()),
+                this.http.get('api/physician.json').toPromise().then(res => res.json()),
+                this.http.get('api/prescriptions.json').toPromise().then(res => res.json()),
+                this.http.get('api/visitations.json').toPromise().then(res => res.json())
+            ).subscribe(data => {
+
+                let api = {
+                    'invoices': data[0]['invoices'],
+                    'labResults': data[1]['tests'],
+                    'patient': function () {
+                        return Object.assign({}, data[2], {imageUrl: self.patients.length ? self.patients[id].imageUrl : 'images/placeholder.svg'});
+                    }(),
+                    'physician': function () {
+                        return Object.assign({}, data[3], {imageUrl: 'images/doctor-01.svg'});
+                    }(),
+                    'prescriptions': data[4]['prescriptions'],
+                    'visitations': data[5]['visitations']
+                };
+
+                resolve(api);
+            })
+        });
+
+
+    }
 
 
 }
