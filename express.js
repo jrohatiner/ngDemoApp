56f1d35317c9c453e4f936810e77db8fc79dd32d
Rey Ramos <ramor11@labcorp.com>
2016-10-14 12:10:22 -0400

    update express

--- express.js
+++ express.js
@@ -1,12 +1,18 @@
+/**
+ * Created by ramor11 on 9/8/2016.
+ */
 // Validates that the dist build was successful
 // by serving the files in the "dist" directory
 // via the simplest possible Express application
+//
+// npm install express compression
 
-var express = require('express');
-var path = require('path');
-var open = require('open');
+var express = require('express'),
+  path = require('path');
+
+var helpers = require('./config/helpers');
 var compression = require('compression');
-var port = 3000;
+var port = 9000;
 var app = express();
 
 app.use(compression());
@@ -20,6 +26,6 @@ app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    console.log('express => http://localhost:' + port);
   }
 });
